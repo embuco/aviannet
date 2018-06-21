7654a4d29e97b1dfc546294b7e8a7c37b6f736a6
Fowler <robert_fowler@fws.gov>
2018-06-21 15:12:19 -0400

    0.1.1.9001
    Removed old update line
    broke off date and location in index page

--- docs/build_site.R
+++ docs/build_site.R
@@ -4,8 +4,8 @@ wd = getwd()
 #Set our working directory.
 setwd(paste0(wd,"/docs"))
 
-#render your sweet site.
-rmarkdown::render_site(encoding = "utf-8")
+#render site.
+rmarkdown::render_site(encoding = "utf-8", quiet = TRUE)
 
 setwd(wd)
 rm(current_version)
