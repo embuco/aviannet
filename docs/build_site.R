18b8b2ddbee8217f5de70ab9bb84b98958b4092e
Fowler <robert_fowler@fws.gov>
2018-06-21 14:52:32 -0400

    Revert "what?"
    This reverts commit 64c1d3c998895b0d697ef64a83196f7374f5c9dd.

---
+++ docs/build_site.R
@@ -0,0 +1,11 @@
+current_version <- "0.1.1"
+wd = getwd()
+
+#Set our working directory.
+setwd(paste0(wd,"/docs"))
+
+#render your sweet site.
+rmarkdown::render_site(encoding = "utf-8")
+
+setwd(wd)
+rm(current_version)
