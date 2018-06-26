727c3bfe148ae279edd9b14e4d249ebbf13f98db
Rob Fowler <robgfowler@gmail.com>
2018-06-26 14:08:27 -0400

    build 0.2.0.9002
    build html

--- docs/build_site.R
+++ docs/build_site.R
@@ -11,6 +11,7 @@ setwd(wd)
 rm(current_version)
 
 # News
+# 0.2.0.9002 - Clean up and add sub-page links
 # 0.2.0.9001 - Added some navigation links to index page
 # 0.2.0.9000 - Changing focus to a Primarty goal of developing annotation workflow
 # 0.1.0 First Rough Draft
